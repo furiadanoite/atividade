d01640cc313d6769d237a226a5ed484aadea3fd9
Estudante Alura <105930120+furiadanoite@users.noreply.github.com>
2023-11-09 08:30:58 -0300

    g

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 function tocaSom(idElementoAudio){
 document.querySelector(idElementoAudio).play();
 }
-const listaDeTeclas=document.querySelectorAll("tecla");
+const listaDeTeclas=document.querySelectorAll(".tecla");
 
 let contador=0;
 while(contador<listaDeTeclas.length){
      const efeito = listaDeTeclas[contador].classlist[1];
      const idAudio = "#som_"+efeito;
      listaDeTeclas[contador].onclick = function(){
-        tocaSom("#som_tecla_aplausos");
         tocaSom(idAudio);
      }
-     contador = contador +1;
+     contador = contador + 1;
      console.log(contador);
 }
 
